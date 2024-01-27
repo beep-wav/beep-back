13f571c8c99d3d55d20a5fc88ce5a4347a77768a
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-01-27 14:51:17 +0100

    import db

--- src/User/module/user.module.ts
+++ src/User/module/user.module.ts
@@ -1,21 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: Number(process.env.PG_PORT),
-      password: process.env.PG_PASSWORD,
-      username: process.env.PG_USER,
-      entities: [],
-      database: process.env.PG_DB,
-      synchronize: true,
-      logging: true,
-    }),
-  ],
-})
+@Module({})
 export class UserModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.PG_PORT),
+      password: process.env.PG_PASSWORD,
+      username: process.env.PG_USER,
+      entities: [],
+      database: process.env.PG_DB,
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
